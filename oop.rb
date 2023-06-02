a7aab9a39475ba873bd18af0a3eb2def13335073
vsanantone <vsanantone@gmail.com>
2023-06-02 12:43:09 -0600

    Update oop.rb

--- oop.rb
+++ oop.rb
@@ -57,3 +57,44 @@ p dragon1.rider
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+   attr: name:, disposition:, age:
+  def initialize(name, disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult = false
+    @is_old = false
+    @has_ring = has_ring?
+  end
+
+   def celebrate_birthday
+     @age += 1
+
+     if @age >= 33
+       @is_adult = true
+     end
+     if @age >= 101
+       @is_old = true
+     end
+   end
+   def has_ring?
+     if @name == "Frodo"
+       @has_ring = true
+     else
+       @has_ring = false
+     end
+   end
+end
+
+hobbit1 = Hobbit.new("Bonkers", "grumpy")
+pp hobbit1
+33.times {hobbit1.celebrate_birthday}
+pp hobbit1
+
+hobbit2 = Hobbit.new("Sam", "friendly")
+pp hobbit2
+
+hobbit3 = Hobbit.new("Frodo", "brave")
+pp hobbit3
